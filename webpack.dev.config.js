0a9ed92e723e8e9e3bfde45724c90b6ae91a320c
Márton Papp <morz@morz.hu>
2015-08-14 11:40:59 +0200

    fix webpack dev config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,24 +1,22 @@
 path = require('path');
 
 var node_dir = __dirname +'/node_modules';
+var react_dist = node_dir + '/react/dist/react.js';
 
 var config = {
   entry: {
-    sample : './samples/samples.jsx'
-  },
-  resolve: {
-    alias: {}
+    sample: './samples/samples.jsx',
   },
   output: {
     path: process.env.NODE_ENV === 'production' ? './dist' : './build',
-    filename: '[name].js'
+    filename: '[name].js',
   },
   module: {
-  	noParse: [new RegExp(/\.min.js$/)],
+    noParse: [new RegExp(/\.min.js$/)],
     loaders: [
-      { test: /\.jsx$/, loader: 'babel-loader' }
-    ]
-  }
+      { test: /\.jsx$/, loader: 'babel-loader' },
+    ],
+  },
 };
 
 module.exports = config;
